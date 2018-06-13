08f076917295441a110f91176b5d69d0d805744c
StuartCharters <charters.stuart@gmail.com>
2018-06-13 15:55:32 +0100

    can update the order after editing

--- sinatra_pizza_shop/pizza_controller.rb
+++ sinatra_pizza_shop/pizza_controller.rb
@@ -35,3 +35,16 @@ post "/pizza-orders/:id/delete" do
   @order.delete()
   erb(:delete)
 end
+
+#EDIT
+get "/pizza-orders/:id/edit"do
+  @order = PizzaOrder.find(params[:id].to_i())
+  erb(:edit)
+end
+
+#UPDATE
+post "/pizza-orders/:id" do
+  @order = PizzaOrder.new(params)
+  @order.update()
+  erb(:update)
+end
